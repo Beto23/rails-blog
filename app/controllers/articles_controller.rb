f9f9425c467da2f694902e2ec19a53d9749f9c19
Beto23 <josee-45@hotmail.com>
2016-04-14 21:20:31 -0500

    Delete an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,26 @@
 class ArticlesController < ApplicationController
   #GET /articles
   def index
+    # Obtiene todos los registros SELECT * FROM articles
     @articles = Article.all
   end
+
   #GET /articles/:id
   def show
+    #Encuentra un registro por id
     @article = Article.find(params[:id])
+    #where
+    #Article.where.not("id = ?", params[:id])
   end
+
   #GET /article/new
   def new
     @article = Article.new
   end
+
   #POST /article
   def create
+    #INSERT INTO
     @article = Article.new(title: params[:article][:title], body:params[:article][:body])
 
     if @article.save
@@ -20,8 +28,13 @@ class ArticlesController < ApplicationController
     else
       render :new
     end
+  end
 
-
-
+  #DELETE /articles/:id
+  def destroy
+    #DELETE FROM articles
+    @article = Article.find(params[:id])
+    @article.destroy #destroy elimina el objeto de la base de datos
+    redirect_to articles_path
   end
 end
