4a0e684d069dc81c943c247e8b081630c47e8645
Beto23 <josee-45@hotmail.com>
2016-04-16 01:25:40 -0500

    Add commets by articles and users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :articles do
-    resources :comments
+    resources :comments, only: [:create, :destroy, :update]
   end
 =begin
   get "/articles" index
